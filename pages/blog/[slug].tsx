2ead988fc612a903273f386c2790f8a387a94f33
Ricky <phangr28@gmail.com>
2021-11-07 10:36:59 +0700

    change fallback path to true

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -41,8 +41,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 	);
 	return {
 		paths: data.map((page: IPost) => ({ params: { slug: page.id } })),
-		fallback: false,
-		revalidate: 10,
+		fallback: true,
 	};
 };
 
